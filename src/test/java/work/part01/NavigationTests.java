775149828e78d0a71a3fd14535f8dd6842547627
Evgania Laletina <laletina.e@gmail.com>
2025-12-02 16:37:58 +0300

    Задание 2 - изучение навигации back () forward ()

---
+++ src/test/java/work/part01/NavigationTests.java
@@ -0,0 +1,31 @@
+package work.part01;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class NavigationTests {
+    @Test
+    void test_navigation() {
+        Configuration.browser = "firefox";
+
+        open("https://google.com");
+        sleep(3000);
+
+        open("https://yandex.ru");
+        sleep(3000);
+
+        open("https://www.specialist.ru/");
+        sleep(3000);
+
+        back();
+        sleep(3000);
+        back();
+        sleep(3000);
+        forward();
+        sleep(3000);
+
+
+    }
+}
