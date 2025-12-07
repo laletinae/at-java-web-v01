749055958cbceb39f3585ce5f99fba3e35700cc3
Evgenia Laletina <laletina.e@gmail.com>
2025-12-07 16:26:02 +0300

    Xpath first example

---
+++ src/test/java/work/part02/SimpleXPathTests.java
@@ -0,0 +1,15 @@
+package work.part02;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SimpleXPathTests {
+    @Test
+    void testPageH1() {
+        open("https://slqamsk.github.io/cases/slflights/v01/");
+
+        $x("h1");
+    }
+}
