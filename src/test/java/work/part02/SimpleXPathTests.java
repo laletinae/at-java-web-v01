28693d6a0a6ed761ae9be5d785204fc6882aeb0a
Evgenia Laletina <laletina.e@gmail.com>
2025-12-07 16:34:29 +0300

    Исправлено задание xPath

--- src/test/java/work/part02/SimpleXPathTests.java
+++ src/test/java/work/part02/SimpleXPathTests.java
@@ -1,15 +1,24 @@
 package work.part02;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleXPathTests {
     @Test
     void testPageH1() {
-        open("https://slqamsk.github.io/cases/slflights/v01/");
+        open("https://slqamsk.github.io/tmp/xPath01.html");
 
-        $x("//h1");
+        //Варивант 1
+        SelenideElement $header = $x("//h1");
+        $header.shouldHave(text("Учебная страница для XPath"));
+
+        //Варивант 2
+        $x("//h1").shouldHave(text("Учебная страница для XPath"));
+
+
     }
 }
