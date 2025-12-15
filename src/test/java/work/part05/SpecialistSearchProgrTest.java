bf2eebb3f963e18dc49fbaae189411ce8cf6bc4e
Evgania Laletina <laletina.e@gmail.com>
2025-12-15 06:46:24 +0300

    Домашнее задание на 21 декабря, AT-1 (обязательное) - поиск курса в форматах обучения свободное ПО на сайте Специалист

---
+++ src/test/java/work/part05/SpecialistSearchProgrTest.java
@@ -0,0 +1,70 @@
+package work.part05;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+/*
+ Домашнее задание на 21 декабря, обязательная часть
+ AT-1 (обязательное):
+ На сайте https://www.specialist.ru/ в модальном окне "Наш сайт использует файлы cookie" нажать "Согласен".
+ Выбрать пункт меню "Форматы обучения", затем "Свободное обучение", затем "Выбрать курс",
+ в поле "Направление" выбрать "Программирование", нажать кнопку "Применить" и
+ убедиться, что на странице есть элемент содержащий текст "Тестирование ПО".
+ */
+public class SpecialistSearchProgrTest {
+    @BeforeAll
+    static void beforeAll(TestInfo test_info) {
+        Configuration.pageLoadTimeout = 120_000;
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browser = "firefox";
+        open("https://www.specialist.ru/");
+        getWebDriver().manage().window().maximize();
+    }
+
+    @Test
+    void searchCourse() {
+        //Принять cookie
+        $("#cookieConsent__ok").click();
+
+        //Пункт верхнего меню
+        SelenideElement $menuFormat = $x("//div[@id='js-mobile-menu']//a[contains(text(),'Форматы обучения')]");
+        $menuFormat.click();
+
+        //Формат "свободное обучение"
+        SelenideElement $linkSvobodnoe = $x("//article[@class='format-article']//a[contains(text(),'Свободное обучение')]");
+        $linkSvobodnoe.click();
+
+        //Кнопка "Выбрать курс"
+        SelenideElement $buttonChoose = $x("//a[@href='#selectCourse']");
+        $buttonChoose.click();
+
+        //В поле "Направление" выбрать "Программирование"
+        SelenideElement $selectNapravl = $x("//select[@id='Filter_CategoriesDirectionFilter']");
+        $selectNapravl.selectOption("Программирование");
+
+        // нажать кнопку "Применить"
+        SelenideElement $buttonPrimen = $x("//button[@id='sendBtn']");
+        $buttonPrimen.click();
+
+        // Убедиться, что на странице есть элемент содержащий текст "Тестирование ПО"
+        // 2 варианта поиска (оба работают одинаково)
+
+        // Вариант поиска 1 (поиск по классу тега <section> и по тексту тега <a>)
+        SelenideElement $testirPo1 = $x("//section[@class='schedule-course-block']//a[contains(text(),'Тестирование ПО')]");
+        $testirPo1.should(exist);
+
+        // Вариант 2 (поиск по классу и тексту тега <a> с использованием and между условиями)
+        SelenideElement $testirPo2 = $x("//a[@class='course-link' and contains(text(),'Тестирование ПО')]");
+        $testirPo2.should(exist);
+
+        sleep (500);
+    }
+}
